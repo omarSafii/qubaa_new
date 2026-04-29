61b8f65276ec313cbfc1ef91cdd6e097d727fc6d
omar <os293170@gmail.com>
2026-04-29 15:29:38 +0300

    fix migration dependency

--- backend/accounts/migrations/0006_create_superuser.py
+++ backend/accounts/migrations/0006_create_superuser.py
@@ -1,19 +1,19 @@
 from django.db import migrations
 
 def create_superuser(apps, schema_editor):
-    User = apps.get_model('auth', 'User')  # 👈 هذا مهم
-
-    if not User.objects.filter(username="hamza").exists():
+    User = apps.get_model('auth', 'User')
+    
+    if not User.objects.filter(username='hamza').exists():
         User.objects.create_superuser(
-            username="hamza",
-            email="hamza@gmail.com",
-            password="Hamza@123"
+            username='hamza',
+            email='hamza@gmail.com',
+            password='Hamza@123'
         )
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0001_initial'),
+        ('auth', '0012_alter_user_first_name_max_length'),
     ]
 
     operations = [
